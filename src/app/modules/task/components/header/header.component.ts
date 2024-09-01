f74ca0e5cdba49cc50cf16ed81143fb1d76d1cac
Sherlyn Moreno <sherlynmoreno05@gmail.com>
2024-09-01 14:21:32 -0500

    Cuarto Commit

---
+++ src/app/modules/task/components/header/header.component.ts
@@ -0,0 +1,50 @@
+// import { TestCourseService } from '@shared/services/test-course.service';
+import { Component, OnInit } from '@angular/core';
+// import { NewTaskService } from '@modules/task/services/new-task.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  menu: Array<{name:string, router:any}> = [
+    {
+      name: 'Link',
+      router: []
+    },
+    {
+      name: 'Requests',
+      router: []
+    },
+    {
+      name: 'Shipments',
+      router: []
+    },
+    {
+      name: 'Invoices',
+      router: []
+    },
+    {
+      name: 'Employees',
+      router: []
+    },
+    {
+      name: 'Settings',
+      router: []
+    }
+  ]
+
+  // constructor(
+  //   public testCourseService:TestCourseService){
+
+  // }
+
+  ngOnInit(): void {
+  }
+
+
+}
+
+
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReplaceSpacePipe } from './pipes/replace-space.pipe';
+import { ImgBrokenDirective } from './directives/img-broken.directive';
 
 
 
 @NgModule({
   declarations: [
-    ReplaceSpacePipe
+    ReplaceSpacePipe,
+    ImgBrokenDirective
   ],
   imports: [
     CommonModule
